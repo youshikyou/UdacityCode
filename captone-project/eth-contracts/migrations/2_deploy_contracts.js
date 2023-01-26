a750039462c4c69d887a079aa702b9be2a2d6a97
youshikyou <35782965+youshikyou@users.noreply.github.com>
2023-01-26 08:17:30 +0100

    Delete captone-project directory

--- captone-project/eth-contracts/migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-const Verifier = artifacts.require("./Verifier.sol");
-const SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
-
-module.exports = async function (deployer) {
-    await deployer.deploy(Verifier);
-    await deployer.deploy(SolnSquareVerifier, Verifier.address);
-};
